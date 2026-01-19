518a21194646db085416f04adc7cb86d444aedf0
lowzhanxiang99 <lowzhanxiang99@gmail.com>
2026-01-19 14:35:48 +0800

    Trying Program.cs

--- InternProject1/Program.cs
+++ InternProject1/Program.cs
@@ -1,36 +1,33 @@
-namespace InternProject1
+﻿using System;
+
+namespace MyFirstApp
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // 1. 打印欢迎语
+            Console.WriteLine("=== Welcome to my  C# practical ===");
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            // 2. 获取用户输入
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
 
-            var app = builder.Build();
+            Console.Write("Enter year of birthday (example 1999): ");
+            string inputYear = Console.ReadLine();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            // 3. 简单的逻辑计算 (计算年龄)
+            int birthYear = int.Parse(inputYear);
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
+            // 4. 输出结果
+            Console.WriteLine($"\nHii, {name}!");
+            Console.WriteLine($"Based of calculation, this year you are {age} age。");
 
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
+            // 5. 防止程序立刻关闭
+            Console.WriteLine("\nPlease any key to exit...");
+            Console.ReadKey();
         }
     }
 }
