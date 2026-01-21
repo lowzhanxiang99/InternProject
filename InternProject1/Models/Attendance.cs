cd7d8f3e76b9bfc84f03f737a356992a95916e4b
lowzhanxiang99 <lowzhanxiang99@gmail.com>
2026-01-21 16:16:57 +0800

    Attendance Report Update

--- InternProject1/Models/Attendance.cs
+++ InternProject1/Models/Attendance.cs
@@ -1,20 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 using System.ComponentModel.DataAnnotations.Schema;
+
+
 
 namespace InternProject1.Models;
 
+
+
 public class Attendance
+
 {
+
     [Key]
+
     public int Attendance_ID { get; set; }
 
+
+
     public DateTime Date { get; set; }
+
     public TimeSpan ClockInTime { get; set; }
+
     public TimeSpan? ClockOutTime { get; set; }
+
     public string? Location_Lat_Long { get; set; }
+
     public string? Status { get; set; }
 
+
+
     public int Employee_ID { get; set; }
+
     [ForeignKey("Employee_ID")]
+
     public Employee? Employee { get; set; }
+
 }
