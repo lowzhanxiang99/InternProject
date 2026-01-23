60bafb3d6c9fe462ca1c1ad535e9a1ef4bd8afce
Ziyu007 <czy051105@gmail.com>
2026-01-23 12:09:45 +0800

    fix

---
+++ InternProject1/Migrations/20260123035354_newDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternProject1.Migrations
+{
+    /// <inheritdoc />
+    public partial class newDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Employee_Name",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ClockInTime",
+                table: "Attendances",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Employee_Name",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ClockInTime",
+                table: "Attendances",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+        }
+    }
+}
