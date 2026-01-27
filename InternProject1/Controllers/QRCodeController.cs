308a226683107d9c9c3fb3b872827fd21c33aa55
lowzhanxiang99 <lowzhanxiang99@gmail.com>
2026-01-27 17:20:43 +0800

    QRCode

---
+++ InternProject1/Controllers/QRCodeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace InternProject1.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Generate(int employeeId)
+        {
+            // This grabs the tunnel URL automatically (e.g., https://xxx.devtunnels.ms)
+            var host = Request.Host.Value;
+            var scheme = Request.Scheme;
+
+            // This creates the link your phone actually needs to follow
+            string loginUrl = $"{scheme}://{host}/Account/AutoLogin?empId={employeeId}&secret=TimeVIA123";
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(loginUrl, QRCodeGenerator.ECCLevel.Q))
+            using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
+            {
+                byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
+                return File(qrCodeAsPngByteArr, "image/png");
+            }
+        }
+    }
+}
